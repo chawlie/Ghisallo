154c8ee25c3defcc7bafb50b5dde06fec16b20a5
chawlie <charleschauvin@Get-out-of-my-NACHOs-3.local>
2014-07-05 20:07:14 -0500

    Blog Styles
    Styling the blog / news section and detail pages so that it looks
    amazing.

--- wp-content/themes/ghisallo/single.php
+++ wp-content/themes/ghisallo/single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
+	<div class="small-12 medium-8 push-2 columns" role="main">
 	
 	<?php do_action('foundationPress_before_content'); ?>
 	
@@ -10,7 +10,6 @@
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 <!-- 				<?php FoundationPress_entry_meta(); ?>
  -->			</header>
-			<?php do_action('foundationPress_post_before_entry_content'); ?>
 			<div class="entry-content">
 			
 			<?php if ( has_post_thumbnail() ): ?>
@@ -25,7 +24,7 @@
 			</div>
 			<footer>
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
-				<p><?php the_tags(); ?></p>
+				<p class="tags"><?php the_tags(); ?></p>
 			</footer>
 			<?php do_action('foundationPress_post_before_comments'); ?>
 			<?php comments_template(); ?>
@@ -36,6 +35,6 @@
 	<?php do_action('foundationPress_after_content'); ?>
 
 	</div>
-	<?php get_sidebar(); ?>
-</div>	
+<!-- 	<?php get_sidebar(); ?>
+ --></div>	
 <?php get_footer(); ?>
